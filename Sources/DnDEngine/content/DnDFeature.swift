160f4a2ba3b462496f8213f1976db6f2d04c9b79
Rodney Aiglstorfer <r@a9r.me>
2022-08-23 02:30:54 +0800

    added

---
+++ Sources/DnDEngine/content/DnDFeature.swift
@@ -0,0 +1,15 @@
+//
+//  DnDFeature.swift
+//  
+//
+//  Created by Rodney Aiglstorfer on 8/23/22.
+//
+
+import Foundation
+
+struct DnDFeature {
+    var name: String
+    var level: Int
+    var description: String
+  // TODO Need way to translate the description into actionable logic.
+}
