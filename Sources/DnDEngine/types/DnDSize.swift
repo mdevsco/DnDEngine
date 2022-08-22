d3c066872365b338a874b4888916ec50280c7c2f
Rodney Aiglstorfer <r@a9r.me>
2022-08-22 20:53:46 +0800

    added sizeInX variables

--- Sources/DnDEngine/types/DnDSize.swift
+++ Sources/DnDEngine/types/DnDSize.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+/// Each creature takes up a different amount of space.
+/// Objects sometimes use the same size categories.
+
+
 enum DnDSize: String {
     case tiny = "Tiny"
     case small = "Small"
@@ -14,4 +18,30 @@ enum DnDSize: String {
     case large = "Large"
     case huge = "Huge"
     case gargantuan = "Gargantuan"
+    
+    /// The effective size in feet that the creature takes up
+    var sizeInFeet: Double {
+        get {
+            switch(self) {
+            case .tiny:
+                return 2.5
+            case .small, .medium:
+                return 5
+            case .large:
+                return 10
+            case .huge:
+                return 15
+            //case: .gargantuan
+            default:
+                return 20
+            }
+        }
+    }
+    
+    /// The effective size in "squares" that the creature takes up
+    var sizeInSquares: Double {
+        get {
+            return self.sizeInFeet / 5
+        }
+    }
 }
