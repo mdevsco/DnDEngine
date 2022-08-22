de2d89d3d612b86c421f00f1948c99029d2c96df
Rodney Aiglstorfer <r@a9r.me>
2022-08-22 20:29:32 +0800

    added

---
+++ Sources/DnDEngine/types/DnDSize.swift
@@ -0,0 +1,17 @@
+//
+//  DnDSize.swift
+//  
+//
+//  Created by Rodney Aiglstorfer on 8/22/22.
+//
+
+import Foundation
+
+enum DnDSize: String {
+    case tiny = "Tiny"
+    case small = "Small"
+    case medium = "Medium"
+    case large = "Large"
+    case huge = "Huge"
+    case gargantuan = "Gargantuan"
+}
