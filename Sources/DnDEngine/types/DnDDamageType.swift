2ac92f0f29dc3ddc9de6e991c61fe90a5b6b4945
Rodney Aiglstorfer <r@a9r.me>
2022-08-23 00:00:44 +0800

    added damage type

---
+++ Sources/DnDEngine/types/DnDDamageType.swift
@@ -0,0 +1,24 @@
+//
+//  DnDDamageType.swift
+//  
+//
+//  Created by Rodney Aiglstorfer on 8/22/22.
+//
+
+import Foundation
+
+enum DnDDamageType: String {
+    case acid = "Acid"
+    case bludgeoning = "Bludgeoning"
+    case cold = "Cold"
+    case fire = "Fire"
+    case force = "Force"
+    case lightning = "Lightning"
+    case necrotic = "Necrotic"
+    case piercing = "Piercing"
+    case poison = "Poison"
+    case psychic = "Psychic"
+    case radiant = "Radiant"
+    case slashing = "Slashing"
+    case thunder = "Thunder"
+}
