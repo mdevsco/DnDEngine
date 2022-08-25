c25d36a975a389f3403bdbabe5f10f2e5d503adf
Rodney Aiglstorfer <r@a9r.me>
2022-08-25 21:49:23 +0800

    added

---
+++ Tests/DnDEngineTests/content/DnDSpellTest.swift
@@ -0,0 +1,20 @@
+//
+//  DnDSpellTest.swift
+//  
+//
+//  Created by Rodney Aiglstorfer on 8/24/22.
+//
+
+import Foundation
+
+import XCTest
+@testable import DnDEngine
+
+class DnDSpellTest: XCTestCase {
+    
+    func testLoadSpellsFromJSON() throws {
+        let spells = DnDSpell.loadFromFile()
+        
+        XCTAssertFalse(spells.isEmpty)
+    }
+}
