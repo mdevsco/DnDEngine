658dbd48788abc5c242fb9a00a0ff3574b7ea2f6
Rodney Aiglstorfer <r@a9r.me>
2022-08-25 23:25:29 +0800

    Added custom description

--- Sources/DnDEngine/core/DnDAbility.swift
+++ Sources/DnDEngine/core/DnDAbility.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct DnDAbility {
+struct DnDAbility: CustomStringConvertible {
     /// Each of a creature’s abilities has a score, a number that defines the
     /// magnitude of that ability. An ability score is not just a measure of innate
     /// capabilities, but also encompasses a creature’s training and competence
@@ -22,9 +22,12 @@ struct DnDAbility {
     /// To determine an ability modifier subtract 10 from the ability score and
     /// then divide the total by 2 (round down).
     var modifier: Int {
-        get {
-            ((score - 10) / 2) + bonus;
-        }
+        ((score - 10) / 2) + bonus;
+    }
+    
+    /// Custom print string for this ability as it might appear on a character sheet
+    var description: String {
+        "\(type.rawValue) - \(score) (\(modifier))"
     }
     
     init(_ type:DnDAbilityType) {
