8f536ae95c00b3ddfbcc702eeb5aa64860b18fba
Rodney Aiglstorfer <r@a9r.me>
2022-08-25 20:47:08 +0800

    added

---
+++ Sources/DnDEngine/types/DnDAbilityType.swift
@@ -0,0 +1,17 @@
+//
+//  DnDAbilityType.swift
+//  
+//
+//  Created by Rodney Aiglstorfer on 8/25/22.
+//
+
+import Foundation
+ 
+enum DnDAbilityType: String {
+    case strength = "Strength"
+    case dexterity = "Dexterity"
+    case constitution = "Constitution"
+    case intelligence = "Intelligence"
+    case wisdom = "Wisdom"
+    case charisma = "Charisma"
+}
