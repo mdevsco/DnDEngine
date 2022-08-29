675ff45709368a1ac8f20cb00d4ee997a1267a83
Rodney Aiglstorfer <r@a9r.me>
2022-08-29 12:29:18 +0800

    added resource

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DnDEngine",
-            dependencies: ["DiceKit"]
+            dependencies: ["DiceKit"],
+            resources: [.process("resources")]
         ),
         
         .testTarget(
