b2c54a3b2cdaed2821cfbc0b43b1412b3eeaabdf
Rodney Aiglstorfer <r@a9r.me>
2022-08-29 22:46:33 +0800

    added

---
+++ Tests/DnDEngineTests/content/DnDLanguageTest.swift
@@ -0,0 +1,21 @@
+//
+//  DnDLanguageTest.swift
+//  
+//
+//  Created by Rodney Aiglstorfer on 8/29/22.
+//
+
+import XCTest
+@testable import DnDEngine
+
+class DnDLanguageTest: XCTestCase {
+
+    func testLoadFromFile() throws {
+        let languages = try DnDLanguage.loadFromFile()
+        
+        XCTAssertFalse(languages.isEmpty)
+        print(languages[0])
+    }
+
+
+}
