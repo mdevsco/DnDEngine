5d47fa41bbcc268f40e5287b57534b8f56a2f3b8
Rodney Aiglstorfer <r@a9r.me>
2022-08-29 22:51:18 +0800

    .gitignore

---
+++ Sources/DnDEngine/combat/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rodney Aiglstorfer on 8/25/22.
+//
+
+import Foundation
---
+++ Tests/DnDEngineTests/types/DnDSpellTest.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rodney Aiglstorfer on 8/24/22.
+//
+
+import Foundation
