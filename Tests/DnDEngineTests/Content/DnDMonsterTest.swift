c7d1edfb0e51f6523215132ef9b82fd0569036df
Rodney Aiglstorfer <r@a9r.me>
2022-09-02 04:25:12 +0800

    added

---
+++ Tests/DnDEngineTests/Content/DnDMonsterTest.swift
@@ -0,0 +1,32 @@
+//
+//  DnDMonsterTest.swift
+//  
+//
+//  Created by Rodney Aiglstorfer on 8/31/22.
+//
+
+import XCTest
+@testable import DnDEngine
+
+class DnDMonsterTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+//    func testDecodeMonsters() throws {
+//        let dNDMonster = try? newJSONDecoder().decode(DNDMonster.self, from: jsonData)
+//    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
