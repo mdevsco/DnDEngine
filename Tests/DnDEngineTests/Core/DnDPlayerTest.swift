f74ffe5d5a1e09a6a18de836c0b9f99555720fe7
Rodney Aiglstorfer <r@a9r.me>
2022-09-02 04:26:59 +0800

    updated

--- Tests/DnDEngineTests/Core/DnDPlayerTest.swift
+++ Tests/DnDEngineTests/Core/DnDPlayerTest.swift
@@ -10,36 +10,79 @@ import Foundation
 
 import XCTest
 @testable import DnDEngine
+@testable import DiceKit
 
 class DnDPlayerTest: XCTestCase {
  
     func testCreateNewPlayer() throws {
-        let player1 = DnDPlayer(name: "Drow Rogue 1")
+        // Create a new instance of the play using its name
+        let p = DnDPlayer(name: "Draken")
+        XCTAssertEqual(p.name, "Draken")
+        XCTAssertNil(p.race)
+        XCTAssertNil(p.classType)
+        // Validate that the correct defaults apply
+        XCTAssertEqual(p.level, 1)
+        XCTAssertEqual(p.xp, 0)
+        XCTAssertEqual(p.proficiencyBonus, 2)
+        // MARK: Validate that defaul speed is Basic 30ft
+        XCTAssertEqual(p.speeds.isEmpty, false)
+        p.speeds.insert(DnDSpeed(120, movement: .fly))
+        XCTAssertEqual(p.speeds.count, 2)
+        XCTAssertEqual(p.speeds.first { speed in
+            speed.movement == .basic
+        }?.amount, 30)
+        XCTAssertEqual(p.speeds.first { speed in
+            speed.movement == .fly
+        }?.amount, 120)
         
-        player1.race = "Elf (Drow)"
-        player1.classType = DnDClassType.rogue
+        // MARK: Validate that all array types are not nil and empty
+        XCTAssertTrue(p.languages.isEmpty)
+        XCTAssertTrue(p.armorProficiencies.isEmpty)
         
-        player1.hitPoints = 8
-        player1.hitDice = "1d8"
-        player1.speed.basic = DnDPace.normal
+        // MARK: Test "Empty" Equipment
+        XCTAssertNil(p.equipment.armor)
+        XCTAssertNil(p.equipment.mainHand)
+        XCTAssertNil(p.equipment.offHand)
+        XCTAssertNil(p.equipment.head)
+        XCTAssertNil(p.equipment.shoulders)
+        XCTAssertNil(p.equipment.chest)
+        XCTAssertNil(p.equipment.wrists)
+        XCTAssertNil(p.equipment.hands)
+        XCTAssertNil(p.equipment.waist)
+        XCTAssertNil(p.equipment.legs)
+        XCTAssertNil(p.equipment.feet)
+        XCTAssertNil(p.equipment.neck)
+        XCTAssertNil(p.equipment.back)
+        XCTAssertTrue(p.equipment.fingers.isEmpty)
+        XCTAssertFalse(p.equipment.isShieldOn)// Computed
+        XCTAssertFalse(p.equipment.isArmorOn) // Computed
+        XCTAssertEqual(p.equipment.weight, 0) // Computed
         
-        player1.strength.score = 8
+        p.race = "Elf (Drow)"
+        p.classType = DnDClassType.rogue
         
-        player1.dexterity.score = 16
-        player1.dexterity.bonuses.append(DnDBonus.racial(2, "Elf (Drow)"))
-        player1.dexterity.bonuses.append(DnDBonus.feat(1, "Athlete"))
+        p.hitPoints = 8
+        p.hitDice = [Die.d10, Die.d8]
         
         
-        player1.constitution.score = 10
         
-        player1.intelligence.score = 13
-        player1.intelligence.bonuses.append(DnDBonus.racial(1, "Elf (Drow)"))
+        p.strength.score = 8
         
-        player1.wisdom.score = 12
+        p.dexterity.score = 16
+        p.dexterity.bonuses.append(DnDBonus.racial(2, "Elf (Drow)"))
+        p.dexterity.bonuses.append(DnDBonus.feat(1, "Athlete"))
         
-        player1.charisma.score = 16
         
-        player1.armorProficiencies.insert(DnDArmorType.light)
-        XCTAssertEqual(player1.passivePerception, 15)
+        p.constitution.score = 10
+        
+        p.intelligence.score = 13
+        p.intelligence.bonuses.append(DnDBonus.racial(1, "Elf (Drow)"))
+        
+        p.wisdom.score = 12
+        
+        p.charisma.score = 16
+        
+        p.armorProficiencies.insert(DnDArmorType.light)
+        XCTAssertEqual(p.passivePerception, 11)
     }
 }
